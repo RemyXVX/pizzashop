4c59a3d449057e4d94261412371853e6317e852b
RemyXVX <remyfranciscoflores@gmail.com>
2023-07-06 02:18:04 -0700

    prepping for deployment

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -64,7 +64,7 @@ const Navigation = () => {
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
-                to="/"
+                to="./"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 aria-current="page"
               >
@@ -73,7 +73,7 @@ const Navigation = () => {
             </li>
             <li>
               <Link
-                to="/Menu"
+                to="./Menu"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Menu
@@ -81,7 +81,7 @@ const Navigation = () => {
             </li>
             <li>
               <Link
-                to="/Order"
+                to="./Order"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Order
@@ -89,14 +89,14 @@ const Navigation = () => {
             </li>
             <li>
               <Link
-                to="/About"
+                to="./About"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 About
               </Link>
             </li>
             <li>
-              <Link to="/Contact" className="py-2">
+              <Link to="./Contact" className="py-2">
                 Contact
               </Link>
             </li>
